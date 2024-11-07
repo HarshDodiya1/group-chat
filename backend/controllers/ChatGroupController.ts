63496e5d5d1a5346d431e37bec319556da9a163b
HarshDodiya1 <dodiyaharsh999@gmail.com>
2024-11-07 12:35:15 +0530

    Implement CRUD operations for chat groups in ChatGroupController; update routes.

--- backend/controllers/ChatGroupController.ts
+++ backend/controllers/ChatGroupController.ts
@@ -7,7 +7,39 @@ interface AuthenticatedRequest extends Request {
 }
 
 class ChatGroupController {
-  static async store(req: AuthenticatedRequest, res: Response): Promise<void> {
+  static async fetchAllChatGroupsByUserId(
+    req: AuthenticatedRequest,
+    res: Response,
+  ): Promise<void> {
+    try {
+      const fetchedChatGroups = await db.chatGroup.findMany({
+        where: {
+          user_id: req.user.id,
+        },
+        orderBy: {
+          created_at: "desc",
+        },
+      });
+
+      res.json({
+        message: "Chat Groups are fetched successfully by userId!",
+        data: fetchedChatGroups,
+      });
+      return;
+    } catch (error) {
+      console.error("Error while fetching chat groups by userId:", error);
+      res.status(500).json({
+        message:
+          "Something went wrong while fetching the chat groups by userId",
+      });
+      return;
+    }
+  }
+
+  static async createChatGroup(
+    req: AuthenticatedRequest,
+    res: Response,
+  ): Promise<void> {
     try {
       const { title, passcode } = req.body;
 
@@ -19,14 +51,92 @@ class ChatGroupController {
         },
       });
 
-      const { passcode: string, ...data } = chatGroupData;
+      const { passcode: string, ...chatGroupDataWithoutPasscode } =
+        chatGroupData;
 
-      res.json({ message: "Chat Group created successfully!", data });
+      res.json({
+        message: "Chat Group created successfully!",
+        data: chatGroupDataWithoutPasscode,
+      });
       return;
     } catch (error) {
       console.error("Error creating chat group:", error);
       res.status(500).json({
         message: "Something went wrong while creating chat group",
+      });
+      return;
+    }
+  }
+
+  static async fetchChatGroupById(req: Request, res: Response): Promise<void> {
+    try {
+      const { id } = req.params;
+      const fetchedChatGroup = await db.chatGroup.findUnique({
+        where: {
+          id: id,
+        },
+      });
+
+      res.json({
+        message: "Chat Group fetched successfully by group_id!",
+        data: fetchedChatGroup,
+      });
+      return;
+    } catch (error) {
+      console.error("Error while fetching chat groups by group_id:", error);
+      res.status(500).json({
+        message:
+          "Something went wrong while fetching the chat groups by group_id",
+      });
+      return;
+    }
+  }
+
+  static async updateChatGroup(req: Request, res: Response): Promise<void> {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const updateChatGroup = await db.chatGroup.update({
+        data: body,
+        where: {
+          id: id,
+        },
+      });
+
+      res.json({
+        message: "Chat Group updated successfully!",
+        data: updateChatGroup,
+      });
+      return;
+    } catch (error) {
+      console.error("Error while updating chat group", error);
+      res.status(500).json({
+        message:
+          "Something went wrong while updating chat group, please try again later",
+      });
+      return;
+    }
+  }
+
+  static async deleteChatGroup(req: Request, res: Response): Promise<void> {
+    try {
+      const { id } = req.params;
+      const deletedChatGroup = await db.chatGroup.delete({
+        where: {
+          id: id,
+        },
+      });
+
+      res.json({
+        message: "Chat Group deleted successfully!",
+        data: deletedChatGroup,
+      });
+      return;
+    } catch (error) {
+      console.error("Error while deleting chat group", error);
+      res.status(500).json({
+        message:
+          "Something went wrong while deleting chat group, please try again later",
       });
       return;
     }
--- backend/routes/ChatGroupRoutes.ts
+++ backend/routes/ChatGroupRoutes.ts
@@ -4,7 +4,27 @@ import AuthMiddleware from "../middleware/AuthMiddleware";
 
 const router = Router();
 
-router.post("/chat-group", AuthMiddleware, ChatGroupController.store as any);
+router
+  .post(
+    "/chat-group",
+    AuthMiddleware,
+    ChatGroupController.createChatGroup as any,
+  )
+  .get(
+    "/chat-group",
+    AuthMiddleware,
+    ChatGroupController.fetchAllChatGroupsByUserId as any,
+  )
+  .get(
+    "/chat-group/:id",
+    AuthMiddleware,
+    ChatGroupController.fetchChatGroupById,
+  )
+  .put("/chat-group/:id", AuthMiddleware, ChatGroupController.updateChatGroup)
+  .delete(
+    "/chat-group/:id",
+    AuthMiddleware,
+    ChatGroupController.deleteChatGroup,
+  );
 
 export { router as chatRoute };
-
